8fb39dd52674486a9f488486d1ac27b4b6944c0d
allc <andrellopes@live.com>
2021-04-02 21:34:45 -0300

    clear build

--- lib/main.dart
+++ lib/main.dart
@@ -82,7 +82,7 @@ class _HomeState extends State<Home> {
                 controller: weightController,
                 validator: (value) {
                   if (value.isEmpty) {
-                    return "Insira seu Peso!";
+                    return "Insira seu Peso !";
                   }
                 },
               ),
