995100134f3deaee43db77d1cd490c0469a0061e
allc <andrellopes@live.com>
2021-04-02 21:25:34 -0300

    clear bluid

--- lib/main.dart
+++ lib/main.dart
@@ -23,7 +23,7 @@ class _HomeState extends State<Home> {
     weightController.text = "";
     heightController.text = "";
     setState(() {
-      _infoText = "Informe seus dados!";
+      _infoText = "Informe seus dados !";
       _formKey = GlobalKey<FormState>();
     });
   }
